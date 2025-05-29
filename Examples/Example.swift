34c52e0161881bd8d8e5c9aae03ad022373195da
Simone De Angelis <simonedeangelis@Simones-MacBook-Pro.local>
2025-05-29 19:17:33 +0200

    added gifs, added correct and faulted snap examples

--- Examples/Example.swift
+++ Examples/Example.swift
@@ -8,19 +8,70 @@
 import SwiftUI
 import CenterSnappingScrollView
 
-struct ContentView: View {
+
+struct FaultedSnapExample: View {
+    let screenWidth = UIScreen.main.bounds.width
     var body: some View {
-        ScrollView(.horizontal) {
-            HStack(spacing: 12) {
-                ForEach(0..<10, id: \.self) { index in
-                    RoundedRectangle(cornerRadius: 12)
-                        .fill(Color.blue.opacity(0.7))
-                        .frame(width: CGFloat(50 + (index % 3) * 30), height: 100)
-                        .snappingItem()
+        VStack{
+            Text("Faulted Snap Behaviour")
+                .font(.largeTitle)
+            ScrollView(.horizontal) {
+                HStack(spacing: 24) {
+                    ForEach(0..<10, id: \.self) { index in
+                        RoundedRectangle(cornerRadius: 4)
+                            .fill(Color.blue.opacity(0.7))
+                            .frame(width:  CGFloat((index + 1) * 20),
+                                   height: 100
+                            )
+                        
+                    }
+                }
+                .scrollTargetLayout()
+            }
+            .scrollIndicators(.hidden)
+            .scrollTargetBehavior(.viewAligned)
+            .contentMargins(.horizontal,
+                            EdgeInsets(top: 0,
+                                       leading: screenWidth / 2 - (20 / 2) ,
+                                       bottom: 0,
+                                       trailing: screenWidth / 2 - (10 * 20 / 2)
+                                      )
+            )
+            .overlay(content: {
+                Rectangle()
+                    .fill(.green)
+                    .frame(width: 5, height: 100)
+            })
+
+        }
+    }
+}
+
+struct CorrectSnapExample : View {
+    var body: some View {
+        VStack{
+            Text("Correct Snap Behaviour")
+                .font(.largeTitle)
+            ScrollView(.horizontal) {
+                HStack(spacing: 24) {
+                    ForEach(0..<10, id: \.self) { index in
+                        RoundedRectangle(cornerRadius: 4)
+                            .fill(Color.blue.opacity(0.7))
+                            .frame(width:  CGFloat((index + 1) * 20),
+                                   height: 100
+                            )
+                            .snappingItem()
+                    }
                 }
             }
+            .scrollIndicators(.hidden)
+            .centerSnappingScroll(spacing: 24, axis: .horizontal)
+            .overlay(content: {
+                Rectangle()
+                    .fill(.green)
+                    .frame(width: 5, height: 100)
+            })
+
         }
-        .centerSnappingScroll(spacing: 12) // accepts also an axis parameter for vertical scroll views
-        .padding()
     }
 }
